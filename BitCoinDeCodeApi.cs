6e1f77c1884c0aaf2ead9f0868a3e887d1ee7b65
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2022-08-23 20:07:46 +0700

    Update BitCoinDeCodeApi.cs

--- BitCoinDeCodeApi.cs
+++ BitCoinDeCodeApi.cs
@@ -1,1 +1,91 @@
+local ScTimeui = Instance.new("ScreenGui")
+local timeframe = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
+local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 
+ScTimeui.Name = "TimerUi"
+ScTimeui.Parent = game.CoreGui
+
+timeframe.Name = "timeframe"
+timeframe.Parent = ScTimeui
+timeframe.BackgroundColor3 = Color3.fromRGB(3, 196, 255)
+timeframe.Position = UDim2.new(0.00511695351, 0, 0.114358321, 0)
+timeframe.Size = UDim2.new(0.153508767, 0, 0.0368487947, 0)
+
+TextLabel.Parent = timeframe
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0, 0, 0.068965517, 0)
+TextLabel.Size = UDim2.new(1, 0, 0.862068951, 0)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = ". . ."
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 18.000
+TextLabel.TextWrapped = true
+
+UITextSizeConstraint.Parent = TextLabel
+UITextSizeConstraint.MaxTextSize = 18
+
+UIAspectRatioConstraint.Parent = timeframe
+UIAspectRatioConstraint.AspectRatio = 7.241
+
+local function MakeDraggable(topbarobject, object)
+	local Dragging = nil
+	local DragInput = nil
+	local DragStart = nil
+	local StartPosition = nil
+
+	local function Update(input)
+		local Delta = input.Position - DragStart
+		local pos =
+			UDim2.new(
+				StartPosition.X.Scale,
+				StartPosition.X.Offset + Delta.X,
+				StartPosition.Y.Scale,
+				StartPosition.Y.Offset + Delta.Y
+			)
+		local Tween = TweenService:Create(object, TweenInfo.new(0.2), {Position = pos})
+		Tween:Play()
+	end
+
+	topbarobject.InputBegan:Connect(
+		function(input)
+			if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+				Dragging = true
+				DragStart = input.Position
+				StartPosition = object.Position
+
+				input.Changed:Connect(
+					function()
+						if input.UserInputState == Enum.UserInputState.End then
+							Dragging = false
+						end
+					end
+				)
+			end
+		end
+	)
+
+	topbarobject.InputChanged:Connect(
+		function(input)
+			if
+				input.UserInputType == Enum.UserInputType.MouseMovement or
+				input.UserInputType == Enum.UserInputType.Touch
+			then
+				DragInput = input
+			end
+		end
+	)
+
+	UserInputService.InputChanged:Connect(
+		function(input)
+			if input == DragInput and Dragging then
+				Update(input)
+			end
+		end
+	)
+end
+
+MakeDraggable(timeframe, timeframe)
