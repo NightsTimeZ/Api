235738c2bf4ae351177b8dcf810eda619e3b8b96
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2023-10-07 03:23:35 +0700

    Update Priority.lua

--- Priority.lua
+++ Priority.lua
@@ -15,46 +15,52 @@ end
 Priority.get = function(name)
     for i,v in ipairs(DataPriority) do 
         if v.Name == name then 
-            local temp = function(f)
-                v.IsActive = f
-                if ww then 
-                    Priority.Recently = name
-                else
-                    Priority.Recently = nil
-                end
+            return v
+        end
+    end
+end
+Priority.set = function(name,ww)
+    for i,v in ipairs(DataPriority) do 
+        if v.Name == name then 
+            v.IsActive = ww
+            if ww then 
+                Priority.Recently = name
+            else
+                Priority.Recently = nil
             end
-            local temp2 = function()
-                local myid = v.PriorityId
-                for i,v2 in ipairs(DataPriority) do 
-                    if v2.Name ~= name then 
-                        local He = v2.PriorityId
-                        local HeSet = v2.IsActive
-                        if (He < myid or He > myid) and HeSet == 1 then 
-                            return true
-                        end
-                        if myid > He and HeSet == 2 then 
-                            return true
-                        end
-                    end
-                end
-                return false
-            end
-            return {data = v,set = temp,CanActive = temp2}
+            
+            break
         end
     end
 end
 Priority.GetClip = function()
     for i,v in ipairs(DataPriority) do 
         if v.RequireClip == true then 
-            return v.IsActive > 1
+            return v.IsActive
         end
     end
 end
+Priority.canactive = function(name)
+    local myid = Priority.get(name).PriorityId
+    for i,v in ipairs(DataPriority) do 
+        if v.Name ~= name then 
+            local He = v.PriorityId
+            local HeSet = v.IsActive
+            if (He < myid or He > myid) and HeSet == 1 then 
+                return true
+            end
+            if He < myid and HeSet == 2 then 
+                return true
+            end
+        end
+    end
+    return true
+end
 Priority.clear = function()
-    Priority.Recently = nil
     for i,v in ipairs(DataPriority) do 
         v.IsActive = 1
     end
 end
+
 
 return Priority
